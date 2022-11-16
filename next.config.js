7b66911b9e0c999babb1fb5ce6fbb7d698d3c791
Chris Jardine <dev@keplux.com>
2022-11-15 22:29:51 -0500

    Turn off ignore ts and eslint errors.

--- next.config.js
+++ next.config.js
@@ -27,10 +27,10 @@ module.exports = {
   },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
-    ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+    ignoreBuildErrors: false,
   },
   eslint: {
     /// Set this to false if you want production builds to abort if there's lint errors
-    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
+    ignoreDuringBuilds: false,
   },
 };
