c2b3ea675f10c0b0d2804af8c740702ae851dfb7
Chris Jardine <chris@keplux.com>
2022-11-17 17:55:34 -0500

    Add theme toggle.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,51 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  Box,
+  ChakraProvider,
+  Button as ChakraButton,
+  Icon,
+} from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
 import { Footer } from '../components/core';
 import { Navbar } from '../components/core/Navbar';
 import '../styles/globals.css';
 import chakraTheme from '../styles/theme/theme';
+import React from 'react';
+import { FaSun } from '@react-icons/all-files/fa/FaSun';
+import { FaMoon } from '@react-icons/all-files/fa/FaMoon';
 
 function MyApp({ Component, pageProps }) {
-  const theme = chakraTheme.lightTheme;
+  const [theme, setTheme] = React.useState(chakraTheme.lightTheme);
   return (
     <ChakraProvider theme={theme}>
       <DefaultSeo titleTemplate={`%s | Lorem Ipsum Blog`} />
+      <Box position="absolute" maxW="8xl" w="full" h="100vh">
+        <Box
+          as={ChakraButton}
+          position="fixed"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          zIndex={100}
+          bg={theme === chakraTheme.lightTheme ? 'yellow.400' : 'purple.900'}
+          shadow="dark-lg"
+          w={14}
+          h={14}
+          rounded="full"
+          bottom={4}
+          right={4}
+          onClick={() => {
+            theme === chakraTheme.lightTheme
+              ? setTheme(chakraTheme.darkTheme)
+              : setTheme(chakraTheme.lightTheme);
+          }}
+        >
+          {theme === chakraTheme.lightTheme ? (
+            <Icon as={FaSun} w={6} h={6} color="white" />
+          ) : (
+            <Icon as={FaMoon} w={6} h={6} color="white" />
+          )}
+        </Box>
+      </Box>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
