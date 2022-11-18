160d4329506f531389170776610eaed54d0c2560
Chris Jardine <chris@keplux.com>
2022-11-17 19:25:30 -0500

    Remove wrapper around theme toggler that's preventing clicking links in some areas.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,33 +18,31 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <DefaultSeo titleTemplate={`%s | Lorem Ipsum Blog`} />
-      <Box position="absolute" maxW="8xl" w="full" h="100vh">
-        <Box
-          as={ChakraButton}
-          position="fixed"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          zIndex={100}
-          bg={theme === chakraTheme.lightTheme ? 'yellow.400' : 'purple.900'}
-          shadow="dark-lg"
-          w={14}
-          h={14}
-          rounded="full"
-          bottom={4}
-          right={4}
-          onClick={() => {
-            theme === chakraTheme.lightTheme
-              ? setTheme(chakraTheme.darkTheme)
-              : setTheme(chakraTheme.lightTheme);
-          }}
-        >
-          {theme === chakraTheme.lightTheme ? (
-            <Icon as={FaSun} w={6} h={6} color="white" />
-          ) : (
-            <Icon as={FaMoon} w={6} h={6} color="white" />
-          )}
-        </Box>
+      <Box
+        as={ChakraButton}
+        position="fixed"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        zIndex={100}
+        bg={theme === chakraTheme.lightTheme ? 'yellow.400' : 'purple.900'}
+        shadow="dark-lg"
+        w={14}
+        h={14}
+        rounded="full"
+        bottom={4}
+        right={4}
+        onClick={() => {
+          theme === chakraTheme.lightTheme
+            ? setTheme(chakraTheme.darkTheme)
+            : setTheme(chakraTheme.lightTheme);
+        }}
+      >
+        {theme === chakraTheme.lightTheme ? (
+          <Icon as={FaSun} w={6} h={6} color="white" />
+        ) : (
+          <Icon as={FaMoon} w={6} h={6} color="white" />
+        )}
       </Box>
       <Navbar />
       <Component {...pageProps} />
